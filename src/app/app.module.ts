7a3e7e7756026acc2d044f842a8660457112258b
Charles Sinde <charles.sinde@gmail.com>
2017-04-25 22:17:04 -0700

    first ocmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthenticationComponent } from './authentication/authentication/authentication.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
